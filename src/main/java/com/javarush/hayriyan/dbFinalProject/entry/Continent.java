c936a586fee48a14a22f6e8f783f157e36e77270
radikhayriyan1 <radik.hayriyan1@gmail.com>
2024-08-04 15:25:50 +0400

    Implemented db final project

---
+++ src/main/java/com/javarush/hayriyan/dbFinalProject/entry/Continent.java
@@ -0,0 +1,11 @@
+package com.javarush.hayriyan.dbFinalProject.entry;
+
+public enum Continent {
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    AFRICA,
+    OCEANIA,
+    ANTARCTICA,
+    SOUTH_AMERICA
+}
